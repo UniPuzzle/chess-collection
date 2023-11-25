d08b4f8e6a1230a10532880edb60f048a7d581d0
VitaliiChichuga <chichygav@gmail.com>
2023-11-25 11:44:28 +0200

    replace files

--- index.js
+++ index.js
@@ -6,13 +6,3 @@ function insertName() {
 }
 
 insertName();
-document.addEventListener('click', e => {
-  const svg = e.target;
-  const index = [...svg.children];
-  for (let i = 0; i < index.length; i++) {
-    const element = index[i];
-
-    element.style.fill = 'red';
-    console.log(element);
-  }
-});
